46ffbb8ae773a3065dc2f6ad196c5142718db3f1
Doru Zanet <doru.zanet@live.com>
2025-11-24 10:56:27 +0200

    added empty orders core projects

---
+++ Sources/Store.Orders.Business/OrdersBusinessLayer.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Store.Orders.Business;
+
+public static class OrdersBusinessLayer
+{
+    public static Assembly Assembly => typeof(OrdersBusinessLayer).Assembly;
+}
